69c44b38acdf92ebe833d5871f754f432c9dd93b
ishmael354 <163950670+ishmael354@users.noreply.github.com>
2024-06-15 16:50:28 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,48 +1,10 @@
-import streamlit as st
+import pandas as pd
 
 def generate_html_with_citations(chat_log):
     html_content = ""
     for chat in chat_log:
-        msg = chat["msg"]
-        citations = chat.get("citations", [])
-        for idx, citation in enumerate(citations):
-            citation_text, citation_source = citation
-            marker = f'<span style="color:blue; cursor:pointer;" title="{citation_source}">[{idx + 1}]</span>'
-            msg = msg.replace(citation_text, marker)
-        html_content += f'<p>{msg}</p>'
+        html_content += f"<div><strong>{chat['name']}:</strong> {chat['msg']}</div>"
     return html_content
 
 def add_tooltip_css():
-    tooltip_css = """
-    <style>
-    .sidebar {
-        position: fixed;
-        left: 0;
-        top: 0;
-        width: 250px;
-        height: 100%;
-        background-color: #f1f1f1;
-        padding-top: 20px;
-        overflow-x: hidden;
-        transition: 0.5s;
-        z-index: 1;
-    }
-    .sidebar a {
-        padding: 10px 15px;
-        text-decoration: none;
-        font-size: 18px;
-        color: #818181;
-        display: block;
-        transition: 0.3s;
-    }
-    .sidebar a:hover {
-        color: #f1f1f1;
-    }
-    .main {
-        margin-left: 260px;
-        transition: margin-left 0.5s;
-        padding: 20px;
-    }
-    </style>
-    """
-    st.markdown(tooltip_css, unsafe_allow_html=True)
+    pass  # No-op for now as we removed the citations
