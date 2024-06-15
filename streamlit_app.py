4d849d83441447e0d1c34f7edb567f2113cc8d3b
ishmael354 <163950670+ishmael354@users.noreply.github.com>
2024-06-15 17:33:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import streamlit as st
+import re
 from openai import AssistantEventHandler
 from typing_extensions import override
 
@@ -9,7 +10,7 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Verify that all required secrets are loaded
 required_secrets = [
-    "ASSISTANT_1_ID", "ASSISTANT_2_ID", "ASSISTANT_3_ID", "ASSISTANT_4_ID",
+    "ASSISTANT1_ID", "ASSISTANT2_ID", "ASSISTANT3_ID", "ASSISTANT4_ID"
 ]
 
 missing_secrets = [secret for secret in required_secrets if secret not in st.secrets]
@@ -26,30 +27,77 @@ assistants = {
 }
 
 assistant_ids = {
-    "assistant_1": st.secrets["ASSISTANT_1_ID"],
-    "assistant_2": st.secrets["ASSISTANT_2_ID"],
-    "assistant_3": st.secrets["ASSISTANT_3_ID"],
-    "assistant_4": st.secrets["ASSISTANT_4_ID"]
+    "assistant_1": st.secrets["ASSISTANT1_ID"],
+    "assistant_2": st.secrets["ASSISTANT2_ID"],
+    "assistant_3": st.secrets["ASSISTANT3_ID"],
+    "assistant_4": st.secrets["ASSISTANT4_ID"]
 }
 
-def create_thread(content):
-    messages = [
-        {
-            "role": "user",
-            "content": content,
-        }
-    ]
-    thread = openai.beta.threads.create()
-    return thread
+class EventHandler(AssistantEventHandler):
+    @override
+    def on_event(self, event):
+        pass
 
-def create_message(thread, content):
-    openai.beta.threads.messages.create(
-        thread_id=thread.id, role="user", content=content
-    )
+    @override
+    def on_text_created(self, text):
+        st.session_state.current_message = ""
+        with st.chat_message("Assistant"):
+            st.session_state.current_markdown = st.empty()
+
+    @override
+    def on_text_delta(self, delta, snapshot):
+        if snapshot.value:
+            st.session_state.current_message = snapshot.value
+            st.session_state.current_markdown.markdown(st.session_state.current_message, True)
+
+    @override
+    def on_text_done(self, text):
+        clean_text = re.sub(r'【[^】]+】', '', text)
+        st.session_state.current_markdown.markdown(clean_text, True)
+        st.session_state.chat_log.append({"name": "assistant", "msg": clean_text})
+
+    @override
+    def on_tool_call_created(self, tool_call):
+        st.session_state.current_tool_input = ""
+        with st.chat_message("Assistant"):
+            st.session_state.current_tool_input_markdown = st.empty()
+
+    @override
+    def on_tool_call_delta(self, delta, snapshot):
+        if 'current_tool_input_markdown' not in st.session_state:
+            with st.chat_message("Assistant"):
+                st.session_state.current_tool_input_markdown = st.empty()
+
+        if delta.type == "code_interpreter":
+            if delta.code_interpreter.input:
+                st.session_state.current_tool_input += delta.code_interpreter.input
+                input_code = f"### code interpreter\ninput:\n```python\n{st.session_state.current_tool_input}\n```"
+                st.session_state.current_tool_input_markdown.markdown(input_code, True)
+
+            if delta.code_interpreter.outputs:
+                for output in delta.code_interpreter.outputs:
+                    if output.type == "logs":
+                        pass
+
+    @override
+    def on_tool_call_done(self, tool_call):
+        st.session_state.tool_calls.append(tool_call)
+        if tool_call.type == "code_interpreter":
+            input_code = f"### code interpreter\ninput:\n```python\n{tool_call.code_interpreter.input}\n```"
+            st.session_state.current_tool_input_markdown.markdown(input_code, True)
+            st.session_state.chat_log.append({"name": "assistant", "msg": input_code})
+            st.session_state.current_tool_input_markdown = None
+            for output in tool_call.code_interpreter.outputs:
+                if output.type == "logs":
+                    output = f"### code interpreter\noutput:\n```\n{output.logs}\n```"
+                    with st.chat_message("Assistant"):
+                        st.markdown(output, True)
+                        st.session_state.chat_log.append({"name": "assistant", "msg": output})
 
 def run_stream(user_input, assistant_id):
     if "thread" not in st.session_state:
-        st.session_state.thread = create_thread(user_input)
+        st.session_state.thread = openai.beta.threads.create()
+        create_message(st.session_state.thread, user_input)
     create_message(st.session_state.thread, user_input)
     with openai.beta.threads.runs.stream(
         thread_id=st.session_state.thread.id,
@@ -58,31 +106,51 @@ def run_stream(user_input, assistant_id):
     ) as stream:
         stream.until_done()
 
+def handle_uploaded_file(uploaded_file):
+    file = openai.files.create(file=uploaded_file, purpose="assistants")
+    return file
+
+def render_chat():
+    for chat in st.session_state.chat_log:
+        with st.chat_message(chat["name"]):
+            st.markdown(chat["msg"], True)
+
+if "tool_calls" not in st.session_state:
+    st.session_state.tool_calls = []
+
+if "chat_log" not in st.session_state:
+    st.session_state.chat_log = []
+
+if "in_progress" not in st.session_state:
+    st.session_state.in_progress = False
+
+def disable_form():
+    st.session_state.in_progress = True
+
 def main():
     st.title("AI Assistant Chat")
-    st.markdown("### Ask questions about your dataset")
-
-    sample_queries = ["What are some trends in this data set?", "Suggest some visualizations to make based on this data", "Tell me some unexpected findings in the data set"]
-    cols = st.columns(len(sample_queries))
-
-    for i, query in enumerate(sample_queries):
-        with cols[i]:
-            if st.button(query):
-                st.session_state.user_msg = query
-
-    assistant_selection = st.sidebar.selectbox(
+    st.write("Ask questions about your dataset")
+    assistant_selection = st.selectbox(
         "Choose an assistant",
         list(assistants.keys()),
         format_func=lambda x: assistants[x]
     )
-    st.session_state.assistant_id = assistant_ids[assistant_selection]
+    assistant_id = assistant_ids[assistant_selection]
 
-    uploaded_file = st.sidebar.file_uploader(
+    st.sidebar.title("Assistant Selector")
+    st.sidebar.selectbox("Choose an assistant", list(assistants.keys()), format_func=lambda x: assistants[x])
+    st.sidebar.file_uploader(
         "Upload a file",
-        type=[
-            "txt", "pdf", "png", "jpg", "jpeg", "csv", "json", "geojson", "xlsx", "xls"
-        ]
+        type=["txt", "pdf", "png", "jpg", "jpeg", "csv", "json", "geojson", "xlsx", "xls"]
     )
+    st.sidebar.button("Download Chat as CSV")
+
+    user_msg = st.chat_input(
+        "What is your query?", on_submit=disable_form, disabled=st.session_state.in_progress
+    )
+
+    if user_msg:
+        st.session_state.user_msg = user_msg
 
     if "user_msg" in st.session_state:
         render_chat()
@@ -90,15 +158,12 @@ def main():
             st.markdown(st.session_state.user_msg, True)
         st.session_state.chat_log.append({"name": "user", "msg": st.session_state.user_msg})
 
-        run_stream(st.session_state.user_msg, st.session_state.assistant_id)
+        run_stream(st.session_state.user_msg, assistant_id)
         st.session_state.in_progress = False
         st.session_state.user_msg = ""
         st.rerun()
 
     render_chat()
-
-    if st.button("Download Chat as CSV"):
-        download_chat_as_csv()
 
 if __name__ == "__main__":
     main()
