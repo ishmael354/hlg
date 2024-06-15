2223f3abfce5cc9b7e2b834fa146045925c83c7e
ishmael354 <163950670+ishmael354@users.noreply.github.com>
2024-06-15 17:28:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import os
 import openai
 import streamlit as st
-from openai import AssistantEventHandler
-from typing_extensions import override
-import re
+from event_handler import EventHandler
 
 # Set OpenAI API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
@@ -33,39 +31,9 @@ assistant_ids = {
     "assistant_4": st.secrets["ASSISTANT4_ID"]
 }
 
-class EventHandler(AssistantEventHandler):
-    @override
-    def on_event(self, event):
-        pass
-
-    @override
-    def on_text_created(self, text):
-        st.session_state.current_message = ""
-        with st.chat_message("Assistant"):
-            st.session_state.current_markdown = st.empty()
-
-    @override
-    def on_text_delta(self, delta, snapshot):
-        if snapshot.value:
-            try:
-                clean_message = re.sub(r'【[^】]+】', '', str(snapshot.value))
-                st.session_state.current_message = clean_message
-                st.session_state.current_markdown.markdown(st.session_state.current_message, True)
-            except Exception as e:
-                st.error(f"Error processing text delta: {e}")
-
-    @override
-    def on_text_done(self, text):
-        try:
-            clean_text = re.sub(r'【[^】]+】', '', str(text))
-            st.session_state.current_markdown.markdown(clean_text, True)
-            st.session_state.chat_log.append({"name": "assistant", "msg": clean_text})
-        except Exception as e:
-            st.error(f"Error processing text done: {e}")
-
 def run_stream(user_input, assistant_id):
     thread = openai.beta.threads.create()
-    message = openai.beta.threads.messages.create(
+    openai.beta.threads.messages.create(
         thread_id=thread.id, role="user", content=user_input
     )
     with openai.beta.threads.runs.stream(
