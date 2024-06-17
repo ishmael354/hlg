4dfbdeba7e23b7f6e9e996172d95a9648684f3d1
ishmael354 <163950670+ishmael354@users.noreply.github.com>
2024-06-17 18:23:10 -0400

    Update event_handler.py

--- event_handler.py
+++ event_handler.py
@@ -39,20 +39,3 @@ class EventHandler(AssistantEventHandler):
             self.on_text_delta(event['data'], event['snapshot'])
         elif event['type'] == 'text_done':
             self.on_text_done(event['data'])
-
-def handle_tool_call(action):
-    function_return = {}
-    args = json.loads(action.function.arguments)
-    match action.function.name:
-        case 'run_command':
-            function_return = {"tool_call_id": action.id, "output": run_command(args['hostname'], args['command'])}
-        case 'print_working_dir_files':
-            path = args.get('path', '')
-            function_return = {"tool_call_id": action.id, "output": print_working_dir_files(path)}
-        case 'read_file':
-            function_return = {"tool_call_id": action.id, "output": read_file(args['path'])}
-        case 'write_file':
-            function_return = {"tool_call_id": action.id, "output": write_file(args['path'], args['contents'])}
-        case _:
-            function_return = {"tool_call_id": action.id, "output": 'Function does not exist'}
-    return function_return
